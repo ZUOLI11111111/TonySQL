4c774c2106f23f7905262dcc77816836b4a74410
ZUOLI11111111 <254054663@qq.com>
2025-04-17 12:29:15 +0800

    Initial commit of log_manager

--- log_manager/log_iterator.go
+++ log_manager/log_iterator.go
@@ -3,3 +3,55 @@ package log_manager
 import (
 	fm "file_manager"
 )
+
+type LogIterator struct {
+	file_manager *fm.FileManager
+	block    *fm.BlockId
+	page         *fm.Page
+	bound        uint64
+	current      uint64
+}
+
+func NewLogIterator(file_manager *fm.FileManager, block_id *fm.BlockId) *LogIterator {
+	log_iterator := &LogIterator{
+		file_manager: file_manager,
+		block:     block_id,
+	}
+	log_iterator.page = file_manager.NewPageByBlockId(block_id.Size())
+	err := log_iterator.MoveToBlock(block_id)
+	return log_iterator
+}
+
+func (log_iterator *LogIterator) MoveToBlock(block *fm.Block_Size) error {
+	_, err := log_iterator.log_manager.Read(block, log_iterator.page)
+	if err != nil {
+		return err
+	}
+	log_iterator.bound = log_iterator.page.GetInt(0)
+	log_iterator.current = log_iterator.bound
+	return nil
+}
+
+func (log_iterator *LogIterator) HasNext() bool {
+	return log_iterator.current < log_iterator.bound || log_iterator.page.GetInt(log_iterator.current) >= 0
+}
+
+func (log_iterator *LogIterator) Next() ([]byte, error) {
+	if !log_iterator.HasNext() {
+		return nil, errors.New("no more logs")
+	}
+	if log_iterator.current >= log_iterator.bound {
+		log_iterator.block = log_iterator.block.NewBlockId(log_iterator.block.FileName(), log_iterator.block.Number() - 1)
+		err := log_iterator.MoveToBlock(log_iterator.block)
+		if err != nil {
+			return nil, err
+		}
+	}
+	tmp := log_iterator.current
+	log_iterator.current += 8 + uint64(log_iterator.page.GetInt(log_iterator.current))
+	return log_iterator.page.GetBytes(tmp), nil
+	
+
+}
+
+
--- log_manager/log_manager.go
+++ log_manager/log_manager.go
@@ -1,25 +1,28 @@
 package log_manager
-
 import (
 	file_manager "file_manager"
 	"sync"
 )
-
-const (
-	UINT64_LEN = 8
-)
-
 type LogManager struct {
 	file_manager *file_manager.FileManager
 	block *file_manager.BlockId
 	page *file_manager.Page
 	file_name string
-	block_size uint64
 	saved_id uint64
 	tail_id uint64
 	mutex sync.Mutex
 }
-
+func (log_manager *LogManager) AppendBlock(file_name string) (*file_manager.BlockId, error) {
+	log_manager.mutex.Lock()
+	defer log_manager.mutex.Unlock()
+	block, err := log_manager.file_manager.Append(file_name)
+	if err != nil {
+		return nil, err
+	}
+	log_manager.file_manager.SetInt(0, log_manager.file_manager.Block_Size())
+	log_manager.file_manager.Write(&block, log_manager.page)
+	return log_manager.block, nil
+}
 func NewLogManager(file_manager *file_manager.FileManager, file_name string) (*LogManager, error) {
 	log_manager := &LogManager{
 		file_manager: file_manager,
@@ -29,10 +32,66 @@ func NewLogManager(file_manager *file_manager.FileManager, file_name string) (*L
 		block_size: file_manager.Block_Size(),
 		tail_id: 0,
 	}
-	
-	
-	if err != nil {
-		return nil, err
+	if file_manager.Size(file_name) <= 0 {
+		block ,err := log_manager.AppendBlock(file_name)
+		if err != nil {
+			return nil, err
+		}
+		log_manager.block = block
+	} else {
+		block, err = log_manager.file_manager.NewBlockId(file_name, uint64(file_manager.Size(file_name) - 1))
+		if err != nil {
+			return nil, err
+		}
+		log_manager.file_manager.Read(block, log_manager.page)
+		log_manager.block = block
 	}
+	
 	 return log_manager, nil
+}
+func (log_manager *LogManager) Flush() error {
+	log_manager.mutex.Lock()
+	defer log_manager.mutex.Unlock()
+	_, err := log_manager.file_manager.Write(log_manager.block, log_manager.page)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+func (log_manager *LogManager) FlushById(id uint64) error {
+	log_manager.mutex.Lock()
+	defer log_manager.mutex.Unlock()
+	if id > log_manager.saved_id {
+		err := log_manager.Flush()
+		if err != nil {
+			return err
+		}
+		log_manager.saved_id = id
+	}
+	return nil
+}
+func (log_manager *LogManager) Append(log []byte) (uint64, error) {
+	log_manager.mutex.Lock()
+	defer log_manager.mutex.Unlock()
+	if uint64(log_manager.file_manager.GetInt(0)) - uint64(8 + uint64(len(log))) < 8 {
+		err := log_manager.Flush()
+		if err != nil {
+			return log_manager.tail_id, err
+		}
+		block, err := log_manager.AppendBlock(log_manager.file_name)
+		if err != nil {
+			return log_manager.tail_id, err
+		}
+		log_manager.block = block
+	}
+	log_manager.tail_id++
+	log_manager.file_manager.SetBytes(uint64(log_manager.file_manager.GetInt(0)) - uint64(8 + uint64(len(log))), log)
+	log_manager.file_manager.SetInt(0, uint64(log_manager.file_manager.GetInt(0)) - uint64(8 + uint64(len(log))))
+	return log_manager.tail_id, nil
+}
+func (log_manager *LogManager) Iterator() (*LogIterator, error) {
+	log_manager.mutex.Lock()
+	defer log_manager.mutex.Unlock()
+	log_manager.Flush()
+	return NewLogIterator(log_manager.file_manager, log_manager.block), nil
 }
